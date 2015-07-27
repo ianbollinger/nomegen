37fe9a57dff397774844de68f7c682c4fe47916d
Ian D. Bollinger <ian.bollinger@gmail.com>
2015-07-27 17:35:43 -0400

    Remove lens as a dependency

--- src/Nomegen/Internal.hs
+++ src/Nomegen/Internal.hs
@@ -38,7 +38,6 @@ module Nomegen.Internal (
     MarkovMap (..),
     ) where
 
-import Control.Arrow (first, second)
 import Control.Monad (mzero)
 import Data.Data (Data, Typeable)
 import qualified Data.Foldable as Foldable
@@ -48,19 +47,14 @@ import GHC.Generics (Generic)
 import GHC.Exts (IsList, Item, fromList, toList)
 
 import Control.DeepSeq (NFData)
-import Control.Lens ((^.))
-import Control.Lens.Cons (
-    Cons, Snoc, _Cons, _Snoc, (<|), (|>), _tail, uncons, unsnoc,
-    )
-import Control.Lens.Each (Each, each)
-import Control.Lens.Empty (AsEmpty)
-import Control.Lens.Iso (Iso', iso)
 import Data.Aeson (FromJSON, ToJSON, (.=), (.:), parseJSON, toJSON)
 import qualified Data.Aeson as Json
 import Data.Hashable (Hashable)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import Data.Sequence (Seq)
+import Data.Sequence (
+    Seq, ViewL ((:<), EmptyL), ViewR ((:>)), (<|), (|>), viewl, viewr,
+    )
 import qualified Data.Sequence as Seq
 import Data.Semigroup (Semigroup)
 import Data.Text (Text)
@@ -135,23 +129,23 @@ countSegments context = Foldable.foldl' doNames mempty
                 f = Just . \case
                     Just x -> Map.insertWith (+) successor 1.0 x
                     Nothing -> Map.singleton successor 1.0
-                Just (predecessor, successor) = unsnoc window
+                predecessor :> successor = viewr window
 
 -- | Sliding windows over a sequence. That is, @windows n xs@ is every
 -- length-/n/ subsequence of /xs/ in order.
 windows :: Int -> Seq a -> Seq (Seq a)
 windows n0 = go 0 mempty
     where
-        go n s ax = case uncons ax of
-            Nothing -> mempty
-            Just (a, as)
+        go n s ax = case viewl ax of
+            EmptyL -> mempty
+            a :< as
                 | n' < n0 -> go n' s' as
                 | n' == n0 -> s' <| go n' s' as
                 | otherwise -> s'' <| go n s'' as
                 where
                     n' = n + 1
                     s' = s |> a
-                    s'' = s' ^. _tail
+                    (_ :< s'') = viewl s'
 
 instance ToJSON Nomicon where
     toJSON Nomicon {..} = Json.object [
@@ -175,31 +169,13 @@ newtype Name = Name { _nameSegments :: Seq Segment }
         Typeable
         )
 
-instance AsEmpty Name
-
-instance Cons Name Name Segment Segment where
-    _Cons = nameIso
-        . _Cons
-        . iso (second Name) (second _nameSegments)
-
-instance Each Name Name Segment Segment where
-    each = nameIso . each
-
 instance IsList Name where
     type Item Name = Segment
     fromList = Name . Seq.fromList
     toList = Foldable.toList . _nameSegments
 
-instance Snoc Name Name Segment Segment where
-    _Snoc = nameIso
-        . _Snoc
-        . iso (first Name) (first _nameSegments)
-
 instance ToJSON Name where
     toJSON = Json.String . nameToText
-
-nameIso :: Iso' Name (Seq Segment)
-nameIso = iso _nameSegments Name
 
 nameToText :: Name -> Text
 nameToText = Text.concat . Foldable.toList . fmap _segmentText . _nameSegments
@@ -222,16 +198,6 @@ newtype Segment = Segment { _segmentText :: Text }
         Typeable
         )
 
-instance AsEmpty Segment
-
-instance Cons Segment Segment Char Char where
-    _Cons = segmentIso
-        . _Cons
-        . iso (second Segment) (second _segmentText)
-
-instance Each Segment Segment Char Char where
-    each = segmentIso . each
-
 instance IsList Segment where
     type Item Segment = Char
     fromList = Segment . fromList
@@ -242,16 +208,8 @@ instance FromJSON Segment where
         Json.String x -> return $ Segment x
         _ -> mzero
 
-instance Snoc Segment Segment Char Char where
-    _Snoc = segmentIso
-        . _Snoc
-        . iso (first Segment) (first _segmentText)
-
 instance ToJSON Segment where
     toJSON = Json.String . _segmentText
-
-segmentIso :: Iso' Segment Text
-segmentIso = iso _segmentText Segment
 
 -- Markov generator ------------------------------------------------------------
 
