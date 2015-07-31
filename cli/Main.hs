50846099856b7ab3a4c67155f59961e9264d5a35
Ian D. Bollinger <ian.bollinger@gmail.com>
2015-07-31 18:27:18 -0400

    Update copyright header

--- cli/Main.hs
+++ cli/Main.hs
@@ -1,12 +1,16 @@
--- Copyright: 2014 Ian D. Bollinger
--- License: BSD3
+-- Copyright 2015 Ian D. Bollinger
+--
+-- Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+-- http://www.apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT
+-- or http://opensource.org/licenses/MIT>, at your option. This file may not be
+-- copied, modified, or distributed except according to those terms.
+
 module Main (
     main,
     ) where
 
 import qualified Nomegen.CLI as CLI
 
--- | TODO: Document.
 main :: IO ()
 main = CLI.main
 
